ad3fa0f1f8c9965735d304cd9186c2252ad9ec83
Miika Sikala <miikasikala96@gmail.com>
2016-07-31 23:02:05 +0300

    Password changing started

--- Controllers/Settings.php
+++ Controllers/Settings.php
@@ -11,6 +11,8 @@ Class Settings {
 		$this->view = new View("settings", $action);
 		
 		switch($action) {
+			case "changePassword";
+				$this->changePassword();
 			case "none":
 				$this->index();
 				break;
@@ -18,6 +20,13 @@ Class Settings {
 				die("An error occurred.");
 				break;
 		}
+	}
+	
+	private function changePassword() {
+		$old_password = $_POST['old_password'];
+		$new_password = $_POST['new_password'];
+		$repeat_new_password = $_POST['repeat_new_password'];
+		$correct_password = $this->model->fetchPassword("");
 	}
 	
 	private function index() {
